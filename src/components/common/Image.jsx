964f14074d80cda1286ea9b80af255e3e5a0d9f3
Adrian Jendo <ajendo@uwaterloo.ca>
2024-01-18 15:24:11 -0500

    Rename log folder

--- src/components/common/Image.jsx
+++ src/components/common/Image.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function Image(props) {
   // eslint-disable-next-line react/prop-types
-  const { src, width, alt, height, ...rest } = props;
-  return <img width={width} alt={alt} src={`/images/designLog/${src}`} />;
+  const { src, width, alt, ...rest } = props;
+  return <img width={width} alt={alt} src={`/images/log/${src}`} />;
 }
 
 export default Image;
