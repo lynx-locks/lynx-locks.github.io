dd49613c59214eb8d276558aa7af32abf9978a63
Adrian Jendo <ajendo@uwaterloo.ca>
2024-01-18 15:24:11 -0500

    Rename log folder

--- src/components/common/LocalVid.jsx
+++ src/components/common/LocalVid.jsx
@@ -5,7 +5,7 @@ function LocalVid({ id }) {
   return (
     <div style={{ display: 'flex', justifyContent: 'center' }}>
       <video width="320" height="240">
-        <source src={`/videos/buildLog/${id}`} type="video/mp4" />
+        <source src={`/videos/log/${id}`} type="video/mp4" />
         <track kind="captions" />
       </video>
     </div>
