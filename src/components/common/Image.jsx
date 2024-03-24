067feaa198f3b6b658f425b9d6d45253fe5fe271
Adrian Jendo <ajendo@uwaterloo.ca>
2024-03-24 02:01:09 -0400

    Max width on image

--- src/components/common/Image.jsx
+++ src/components/common/Image.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Image(props) {
   // eslint-disable-next-line react/prop-types
   const { src, width, alt, ...rest } = props;
-  return <div style={{display: 'flex', justifyContent: "center", padding: "20px"}}><img width={width} alt={alt} src={`/images/log/${src}`} /></div>;
+  return <div style={{display: 'flex', justifyContent: "center", padding: "20px"}}><img width={width} alt={alt} src={`/images/log/${src}`} style={{maxWidth: "80%"}}/></div>;
 }
 
 export default Image;
